99d059ac789cbdcfcdd062ba95de9686d50f7db2
Amjad Al Omani <iamjaadk@gmail.com>
2025-12-28 01:39:39 +0000

    upload the files for tasks

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -3,6 +3,7 @@
 after each line containing a specific string in a file.
 """
 
+
 def append_after(filename="", search_string="", new_string=""):
     """Insert a line of text after each line containing a specific string."""
     # اقرأ كل الأسطر
